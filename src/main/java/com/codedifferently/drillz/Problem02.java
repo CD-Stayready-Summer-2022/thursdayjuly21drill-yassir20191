4db33fe591de903a2f8a0f7456b61af177cc51b6
Yas Wood <yaswood@Yass-MacBook-Air.local>
2022-07-21 09:56:15 -0400

    3/3

--- src/main/java/com/codedifferently/drillz/Problem02.java
+++ src/main/java/com/codedifferently/drillz/Problem02.java
@@ -12,7 +12,21 @@ public class Problem02 {
      * @return
      */
     public boolean icyHot(int temp1, int temp2) {
-       if(temp1 < 0 )
+      if(temp1 < 0 && temp2 > 100 ){
+              return true;
+          }
+      else if(temp1 > 100 && temp2 < 0){
+          return true;
+      }
+      else if(temp2 > 100 && temp1 < 0){
+          return true;
+      }
+      else if(temp2 < 0  && temp1 > 100){
+          return true;
+      }
+      else{
+          return false;
+      }
     }
 
 }
